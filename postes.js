75ab0406cb2f2722b6838ded58d0188829327456
Tobi <toblich@gmail.com>
2015-07-10 15:31:00 -0300

    Sacado el brillo especular de los postes

--- postes.js
+++ postes.js
@@ -59,6 +59,7 @@ function postes(){
 	}
 
 	this.draw = function(modelMatrix, gl, shaderProgramSimple, shaderProgramReflection, shaderProgramSoloTexturas){
+		var ks = 0.0;
 		gl.useProgram(shaderProgramReflection);
 	    gl.uniformMatrix4fv(gl.shaderProgramReflection.perspectiveMatrixUniform, false, perspectiveMatrix);
 	    gl.uniformMatrix4fv(gl.shaderProgramReflection.viewMatrixUniform, false, cameraMatrix );
@@ -73,7 +74,7 @@ function postes(){
 	    mat4.translate(matrix_posteUno, matrix_posteUno, [lamparasX , 18.3, lamparaZ1]);
 	    mat4.scale(matrix_posteUno, matrix_posteUno, [0.3, 3.5, 0.3]);
 	    mat4.rotate(matrix_posteUno, matrix_posteUno, degToRad(90.0), [1.0, 0.0, 0.0]);
-	    this.posteUno.drawConTextura(matrix_posteUno, gl, shaderProgramReflection, KA, KD, 0.3, S, shaderProgramSoloTexturas, null);
+	    this.posteUno.drawConTextura(matrix_posteUno, gl, shaderProgramReflection, KA, KD, ks, S, shaderProgramSoloTexturas, null);
 	    // drawConTextura(modelMatrix, gl, shaderProgram, KA, KD, 0.3, S, shaderProgramSoloTexturas, shaderRelieve)
 
 	   	var matrix_posteDos = mat4.create();
@@ -82,7 +83,7 @@ function postes(){
 	    mat4.translate(matrix_posteDos, matrix_posteDos, [lamparasX , 18.3, lamparaZ2]);
 	    mat4.scale(matrix_posteDos, matrix_posteDos, [0.3, 3.5, 0.3]);
    	    mat4.rotate(matrix_posteDos, matrix_posteDos, degToRad(90.0), [1.0, 0.0, 0.0]);
-	    this.posteDos.drawConTextura(matrix_posteDos, gl, shaderProgramReflection, KA, KD, 0.3, S, shaderProgramSoloTexturas, null);
+	    this.posteDos.drawConTextura(matrix_posteDos, gl, shaderProgramReflection, KA, KD, ks, S, shaderProgramSoloTexturas, null);
 
 
 	    gl.useProgram(shaderProgramSimple);
